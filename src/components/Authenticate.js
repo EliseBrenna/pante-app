6bd435f084e6772d246d14c8dc24d55ccb7dadb7
superelise <57939154+EliseBrenna@users.noreply.github.com>
2019-12-20 15:18:18 +0100

    teste out logo

--- src/components/Authenticate.js
+++ src/components/Authenticate.js
@@ -4,7 +4,9 @@ import React from 'react';
 class Authenticate extends React.Component {
     render() {
         return (
-            <div className="test">Authenticate</div>
+            <div>
+                <img className="logo" src="./logo.svg" />
+            </div>
         )
     }
 }
