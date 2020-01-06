85a10ca7dcb7c4b1edfeb91d1e17fe2762729bb5
superelise <57939154+EliseBrenna@users.noreply.github.com>
2020-01-06 09:05:27 +0100

    no message

--- src/components/Authenticate.js
+++ src/components/Authenticate.js
@@ -4,9 +4,12 @@ import React from 'react';
 class Authenticate extends React.Component {
     render() {
         return (
-            <div>
-                <img className="logo" src="./logo.svg" />
+            <div className="authenticate">
+                <div className="logo" >
+                    <img src="./logo.svg" />
+                </div>
             </div>
+                
         )
     }
 }
