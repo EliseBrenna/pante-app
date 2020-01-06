1b82e00860408114700fd9d52c1d980e444d1d04
Therese <brobacktherese@gmail.com>
2020-01-06 15:09:31 +0100

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,20 @@ import React from 'react';
 class Login extends React.Component {
     render() {
         return (
-            <div>Login</div>
+            <div className="login">
+                <img src="./logo.png" alt="logo" className="logoLogin" />
+                <div className="subHeaderLogin"><h3>Logg inn for å pante</h3></div>
+                <div className="loginForm">
+                    <label className="inputField">
+                        <input type="text" placeholder="Skriv inn e-postadresse" />
+                        <input type="text" placeholder="Skriv inn passord" />
+                    </label>
+                </div>
+                <button className="loginBtn">Logg inn</button>
+                <div className="newUser">
+                    <h4><a href="/#/signup">Ny bruker?</a></h4>
+                </div>
+            </div>
         )
     }
 }
