f706b878ffbdfd046d7f81f444449cdf36009af8
superelise <57939154+EliseBrenna@users.noreply.github.com>
2020-01-06 15:10:19 +0100

    Edited CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,19 @@ class Home extends React.Component {
     render() {
         return (
             <div className="home">
+                <div className="home-content">
+                Tast inn kode fra <br/>
+                panteautomaten:
+                    <label className="pin-input">
+                        <input type="text"></input>
+                        <input type="text"></input>
+                        <input type="text"></input>
+                        <input type="text"></input>
+                    </label>
+                    <button>Pant</button>
+                </div>
+                
                 <footer className="nav-bar">
-                    hello
                 </footer>
             </div>
         )
