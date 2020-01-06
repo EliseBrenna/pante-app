4801a5c5ccde48260d0f85c989abe8e3170af3d8
Therese <brobacktherese@gmail.com>
2020-01-06 15:19:03 +0100

    new home icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,6 +17,9 @@ class Home extends React.Component {
                 </div>
                 
                 <footer className="nav-bar">
+                    <div className="home">
+                    <svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 10 21 L 10 15 L 14 15 L 14 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z"/></svg>
+                    </div>
                 </footer>
             </div>
         )
