81cb01b3a169b5b997cca5a16c156da5c03b533a
Therese <brobacktherese@gmail.com>
2020-01-07 11:27:03 +0100

    history page

--- src/components/Support.js
+++ src/components/Support.js
@@ -3,7 +3,22 @@ import React from 'react';
 class Support extends React.Component {
     render() {
         return (
-            <div>Support
+            <div className="helpContainer">
+            <div className="supportContainer">
+                <div className="support">
+                    <h3>Hjelp og kundeservice</h3>
+                </div>
+                <div className="supportOptions">
+                    <p>Ofte stilte spørsmål</p>
+                    <p>Kontakt oss</p>
+                </div>
+                <div className="supportArrows">
+                    <p>></p>
+                    <p>></p>
+                </div>
+                </div>
+                    
+                
                  <footer className="nav-bar">
                     <div className="homeIcon">
                         <svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 10 21 L 10 15 L 14 15 L 14 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z"/></svg>
