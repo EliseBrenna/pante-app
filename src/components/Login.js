c806e959e6bbf86a28fabaf4cdb3dd33eaf8df43
Aske <askeda@gmail.com>
2020-01-07 09:20:20 +0100

    Functions added to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,54 @@
 import React from 'react';
+import { createSession } from '../services/session';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loginForm: {
+                email: '',
+                password: ''
+            },
+            isLoggingIn: false,
+            error: null
+        }
+    }
+
+    handleInputChange(field, event) {
+        this.setState({
+            loginForm: {
+                ...this.state.loginForm,
+                [field]: event.target.value
+            }
+        })
+    }
+
+    async handleLoginAttempt(event) {
+        event.preventDefault();
+        const { history } = this.props;
+        const { email, password } = this.state.loginForm;
+
+        try{
+            this.setState({ isLoggingIn: true, error: null })
+            const { token, error } = await createSession({ email, password })
+
+            if(error) {
+                throw new Error(error)
+            }
+
+            if(!token) {
+                throw new Error('No token recieved - try again');
+            }
+
+            localStorage.setItem('pante_app_token', token);
+            history.push('/')
+        } catch(error) {
+            this.setState({ error, isLoggingIn: false })
+        }
+
+
+    }
     render() {
         return (
             <div className="login">
@@ -8,8 +56,16 @@ class Login extends React.Component {
                 <div className="subHeaderLogin"><h3>Logg inn for å pante</h3></div>
                 <div className="loginForm">
                     <label className="inputField">
-                        <input type="text" placeholder="Skriv inn e-postadresse" />
-                        <input type="text" placeholder="Skriv inn passord" />
+                        <input 
+                        type="text" 
+                        placeholder="Skriv inn e-postadresse"
+                        value={this.state.loginForm.email}
+                        onChange={this.handleInputChange.bind(this, 'email')} />
+                        <input 
+                        type="password" 
+                        placeholder="Skriv inn passord"
+                        value={this.state.loginForm.password}
+                        onChange={this.handleInputChange.bind(this, 'password')} />
                     </label>
                 </div>
                 <button className="loginBtn">Logg inn</button>
