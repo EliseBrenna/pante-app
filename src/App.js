88e193d0c31689da27985c11b422489d02fbece2
Therese <brobacktherese@gmail.com>
2020-01-09 12:51:54 +0100

    Scrolltotop + edit form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route, ScrollToTop } from 'react-router-dom';
 
 import Authenticate from './components/Authenticate';
 import Home from './components/Home';
@@ -12,7 +12,6 @@ import Pant from './components/Pant';
 import History from './components/History';
 import EditProfile from './components/EditProfile';
 import FAQ from './components/FAQ';
-
 import './App.css';
 
 class App extends React.Component {
@@ -20,20 +19,21 @@ class App extends React.Component {
   render() {
     return (
       <HashRouter>
-      <Switch>
-        <Route path="/" exact component={Authenticate} />
-        <Route path="/home" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/logout" component={Logout} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/support" component={Support} />
-        <Route path="/pant" exact component={Pant} />
-        <Route path="/history" component={History} />
-        <Route path="/editprofile" component={EditProfile} />
-        <Route path="/faq" component={FAQ} />
-      </Switch>
-    </HashRouter>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={Authenticate} />
+          <Route path="/home" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/support" component={Support} />
+          <Route path="/pant" exact component={Pant} />
+          <Route path="/history" component={History} />
+          <Route path="/editprofile" component={EditProfile} />
+          <Route path="/faq" component={FAQ} />
+        </Switch>
+      </HashRouter>
     )
   }
 }
