39ec79bc8a8c9709f82c006976738bbd360cf7c6
Aske <askeda@gmail.com>
2020-01-16 09:54:00 +0100

    Fixed validation input

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -54,7 +54,15 @@ class Signup extends React.Component {
             this.setState({ error: "Passordet ikke gyldig. Minimum 8 tegn, minst en bokstav og et tall påkrevd", passwordClass: 'invalid' })
         } else if (confirmPassword !== password) {
             this.setState({ error: "Passordene du har skrevet inn matcher ikke", passwordClass: 'invalid', confirmPasswordClass: 'invalid' })
-        } 
+        }
+        
+        this.setState({
+            isLoading: true,
+            EmailClass: '', 
+            passwordClass: '', 
+            confirmPasswordClass: '',
+            error: null,
+        })
         
         if (emailTest(email) && passwordTest(password) && confirmPassword === password) {    
             try {
